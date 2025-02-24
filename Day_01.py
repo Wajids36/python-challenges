d2a24737c87e0388a733e1853c9877b5fc30c267
Wajids36 <161895021+Wajids36@users.noreply.github.com>
2025-02-25 04:34:51 +0500

    Add files via upload

---
+++ Day_01.py
@@ -0,0 +1,9 @@
+# Day 01- Python challenge: Print Your First Message! 
+
+# Apna Pehla Python Program Likho Jo Screen par Ye Print kare: 
+
+#  "Hello Python Learners! Today is the first step in our coding journey."
+
+
+my_name= "Wajid Ali"
+print(f"Hello, Python learner, I'm {my_name} Today is the first step in our coding journey.")
